4510e4e3ed75a7b3808992d8d0749755bef09187
h28p <unjeong25.11226@ggh.goe.go.kr>
2025-08-28 01:27:02 +0900

    Update bellman-ford.py

--- bellman-ford.py
+++ bellman-ford.py
@@ -5,23 +5,27 @@ from sys import stdin
 input = stdin.readline
 
 # 입력
+
 N, M = map(int, input().split())
 graph = []
 INF = 10**8
 dist = [INF] * (N+1)
 dist[1] = 0
 isLoopExist = False
-
+# 리스트에 간선을 차례대로 저장
 for i in range(M):
     A, B, C = map(int, input().split())
     graph.append((A, B, C))
 
 # 메인 코드
+
 for i in range(1, N+1):
+    # N-1번 반복 : 모든 간선에 대해 갱신하기 -> 전부 갱신됨
     if i < N:
         for u, v, w in graph:
             if dist[u] != INF:
                 dist[v] = min(dist[v], dist[u]+w)
+    # N번째 : 이때도 갱신되면 음수 사이클이 존재함 -> 최단거리 계산 불가능
     else:
         for u, v, w in graph:
             if dist[u] != INF and dist[v] > dist[u] + w:
@@ -38,4 +42,5 @@ else:
             print(-1)
         else:
             print(i)
+
 
